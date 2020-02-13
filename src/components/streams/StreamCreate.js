b811b701e72ea76cc42f7a5e64fffad30c581a40
Beta Dayz <betadayz888@gmail.com>
2020-02-13 15:35:12 +0100

    creating bulk action

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends Component {
     renderError({ error, touched }) {
@@ -22,9 +24,9 @@ class StreamCreate extends Component {
          );
     };
 
-    onSubmit(formValue) {
-        console.log(formValue)
-    }
+    onSubmit = (formValues) => {
+        this.props.createStream(formValues)
+    };
 
     render() {
         return (
@@ -54,7 +56,9 @@ const validate = (formValue) => {
 };
 
 
-export default reduxForm({
+const formWrapped = reduxForm({
     form: 'streamCreate',
     validate
 })(StreamCreate)
+
+export default connect(null, { createStream })(formWrapped);

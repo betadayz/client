46bf564705f57a9fa9fe799877db7293714cd520
Beta Dayz <betadayz888@gmail.com>
2020-02-14 12:11:45 +0100

    Rendering edit and delete button on the stream

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -7,10 +7,22 @@ import { fetchStreams } from '../../actions';
          this.props.fetchStreams()
      }
 
+     renderAdmin(stream) {
+         if (stream.userId === this.props.currentUserId) {
+             return (
+                 <div className="right floated content">
+                     <button className="ui button primary">Edit</button>
+                     <button className="ui button negative">Delete</button>
+                 </div>
+                )
+         }
+     }
+
      renderList() {
          return this.props.streams.map((stream) => {
              return (
                  <div className="item" key={stream.id}>
+                   {this.renderAdmin(stream)}
                      <i className="large middle aligned icon camera" />
                      <div className="content">
                          {stream.title}
@@ -22,7 +34,6 @@ import { fetchStreams } from '../../actions';
      };
 
     render() {
-        console.log(this.props.streams)
         return (
             <div>
                 <h2>Streams</h2>
@@ -33,7 +44,10 @@ import { fetchStreams } from '../../actions';
 };
 
 const mapStateToProps = (state) => {
-    return { streams: Object.values(state.streams)}
+    return { 
+        streams: Object.values(state.streams),
+        currentUserId: state.auth.userId
+    }
 }
 
-export default connect(mapStateToProps, { fetchStreams }) (StreamList);
+export default connect(mapStateToProps, {fetchStreams}) (StreamList);

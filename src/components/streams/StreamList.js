c59c2da645ebd9d458f67b6f164d9c28f41edc6f
Beta Dayz <betadayz888@gmail.com>
2020-04-10 17:54:50 +0100

    commentting about the fetching of data from database

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import { fetchStreams } from '../../actions';
 
  class StreamList extends Component {
+
+     // fetching data from the database
      componentDidMount() {
          this.props.fetchStreams()
      }

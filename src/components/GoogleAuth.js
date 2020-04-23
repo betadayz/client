e7acbff197a8d069228b19c6b83eadf6e5a6e9b3
Beta Dayz <betadayz888@gmail.com>
2020-04-23 10:29:29 +0100

    google auth update

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import { signIn, signOut } from '../actions';
 
-// Google Authentication login
+
 class GoogleAuth extends Component {
     componentDidMount() {
         window.gapi.load('client: auth2', () => {
